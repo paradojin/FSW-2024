86c1c40646faba537f05b7201defbf5241138294
Gonzalo <gonzalo.lazen@sansano.usm.cl>
2024-09-02 19:47:00 -0400

    Sprint 1

---
+++ Sprint1/Backend/codprueba.py
@@ -0,0 +1,243 @@
+from fastapi import FastAPI, UploadFile, File, HTTPException, Form
+from fastapi.responses import JSONResponse
+import cv2
+import numpy as np
+import dlib
+import joblib
+from scipy.spatial import distance as dist
+from imutils import face_utils
+from tensorflow.keras.models import load_model
+from collections import deque
+import time
+import logging
+
+# Configurar el registro de errores
+logging.basicConfig(level=logging.INFO)
+
+app = FastAPI()
+
+# Inicializar el detector de rostros y el predictor de puntos faciales de dlib
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+
+# Cargar los modelos entrenados
+modelo_bostezos_mar = joblib.load('modelo_bostezos_mar.pkl')
+model_pestanear = load_model("eye_status_model.h5")
+
+# Índices de los puntos faciales correspondientes a los ojos y la boca
+(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+(rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+(mStart, mEnd) = face_utils.FACIAL_LANDMARKS_IDXS["mouth"]
+
+# Para almacenar las métricas de somnolencia
+blink_counter = 0
+blink_timestamps = deque()
+yawn_timestamps = deque()
+total_blinks = 0
+total_yawns = 0
+microsuenos_acumulados = 0
+start_time = time.time()
+
+# Variables para medir el tiempo de los ojos cerrados
+microsleep_start_time = None
+left_eye_closed_start_time = None
+right_eye_closed_start_time = None
+mouth_open_start_time = None
+
+def eye_aspect_ratio(eye):
+    A = np.linalg.norm(eye[1] - eye[5])
+    B = np.linalg.norm(eye[2] - eye[4])
+    C = np.linalg.norm(eye[0] - eye[3])
+    ear = (A + B) / (2.0 * C)
+    return ear
+
+def eye_status(eye):
+    if eye.size == 0:
+        return "Unknown"
+    eye = cv2.resize(eye, (24, 24))
+    eye = eye.astype("float") / 255.0
+    eye = np.expand_dims(eye, axis=-1)
+    eye = np.expand_dims(eye, axis=0)
+    pred = model_pestanear.predict(eye)
+    return "Closed" if pred > 0.6 else "Open"
+
+def mouth_aspect_ratio(mouth):
+    A = dist.euclidean(mouth[13], mouth[19])
+    B = dist.euclidean(mouth[14], mouth[18])
+    C = dist.euclidean(mouth[15], mouth[17])
+    mar = (A + B + C) / (3.0 * dist.euclidean(mouth[12], mouth[16]))
+    return mar
+
+def detectar_bostezo_mar(mar):
+    return int(modelo_bostezos_mar.predict([[mar]])[0])  # Convertir a int
+
+def calcular_puntuacion_somnolencia(microsuenos_acumulados, promedio_pestaneos, bostezos, pesos):
+    puntuacion = (pesos['microsuenos'] * microsuenos_acumulados) + \
+                 (pesos['promedio_pestaneos'] * promedio_pestaneos) + \
+                 (pesos['bostezos'] * bostezos)
+    return min(puntuacion, 100)
+    
+
+def yuv_to_rgb(y, u, v, width, height, format_type='420p'):
+    try:
+        yuv = np.zeros((height + height // 2, width), dtype=np.uint8)
+        yuv[0:height, 0:width] = y.reshape(height, width)
+        u_reshaped = u[: (width // 2) * (height // 2)].reshape((height // 2), (width // 2))
+        v_reshaped = v[: (width // 2) * (height // 2)].reshape((height // 2), (width // 2))
+        yuv[height:height + height // 2, 0:width:2] = u_reshaped
+        yuv[height:height + height // 2, 1:width:2] = v_reshaped
+        rgb_image = cv2.cvtColor(yuv, cv2.COLOR_YUV2BGR_I420)
+    
+    except Exception as e:
+        raise ValueError(f"Error processing YUV data: {str(e)}")
+
+    return rgb_image
+
+
+@app.get("/ping/")
+async def ping():
+    return JSONResponse(content={"status": "ok", "message": "Server is ready to receive requests"})
+
+@app.get("/end_trip/")
+async def end_trip():
+    global blink_timestamps, yawn_timestamps, total_blinks, total_yawns, microsuenos_acumulados, start_time
+    global left_eye_closed_start_time, right_eye_closed_start_time
+
+    # Resetear las variables globales
+    blink_timestamps.clear()
+    yawn_timestamps.clear()
+    total_blinks = 0
+    total_yawns = 0
+    microsuenos_acumulados = 0
+    start_time = time.time()  # Reiniciar el tiempo de inicio
+    left_eye_closed_start_time = None
+    right_eye_closed_start_time = None
+
+    return JSONResponse(content={"status": "ok", "message": "Viaje finalizado y variables reseteadas."})
+
+@app.post("/detect/")
+async def detect(y_plane: UploadFile = File(...), u_plane: UploadFile = File(...), v_plane: UploadFile = File(...),
+                 width: int = Form(...), height: int = Form(...), format_type: str = Form('420p')):
+    try:
+        # Leer los planos Y, U, V
+        y = np.frombuffer(await y_plane.read(), dtype=np.uint8)
+        u = np.frombuffer(await u_plane.read(), dtype=np.uint8)
+        v = np.frombuffer(await v_plane.read(), dtype=np.uint8)
+
+        # Convertir YUV a RGB
+        frame = yuv_to_rgb(y, u, v, width, height, format_type)
+
+        if frame is None:
+            raise HTTPException(status_code=400, detail="Error decoding the image")
+
+        # Asegurarse de que la imagen esté orientada correctamente (rotarla si es necesario)
+        #frame = cv2.rotate(frame, cv2.ROTATE_90_COUNTERCLOCKWISE)    # Para dispositivo fisico 
+        frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)          # Para emulador
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # Detectar rostros y puntos faciales
+        rects, shapes = detectar_rostros_y_puntos(gray)
+
+        # Verificar si se detectaron rostros
+        if len(rects) == 0:
+            raise HTTPException(status_code=400, detail="No se detectaron rostros en la imagen")
+
+        results = []
+
+        global left_eye_closed_start_time, right_eye_closed_start_time, microsleep_start_time
+        for shape in shapes:
+            leftEyeStatus, rightEyeStatus = procesar_ojos(frame, gray, shape)
+
+            # Procesar los ojos
+            global total_blinks, microsuenos_acumulados,blink_counter
+            current_time = time.time()
+            microsleep_threshold = 0.5
+
+           
+            if leftEyeStatus == "Closed" and rightEyeStatus == "Closed":
+                if microsleep_start_time is None:
+                    microsleep_start_time = current_time
+                blink_counter += 1
+                if current_time - microsleep_start_time >= microsleep_threshold:
+                    microsuenos_acumulados += 1  # Incrementamos el total de microsueños
+            else:
+                microsleep_start_time = None
+                if blink_counter > 1:
+                    total_blinks += 1
+                    blink_timestamps.append(current_time)
+                blink_counter = 0
+
+
+            # Procesar la boca
+            global total_yawns, mouth_open_start_time
+            mar = mouth_aspect_ratio(shape[mStart:mEnd])
+            yawn_detected = detectar_bostezo_mar(mar)
+
+            if yawn_detected:
+                if mouth_open_start_time is None:
+                    mouth_open_start_time = time.time()
+                elif (time.time() - mouth_open_start_time) >= 4.0:
+                    total_yawns += 1
+                    yawn_timestamps.append(current_time)
+                    mouth_open_start_time = None
+            else:
+                mouth_open_start_time = None
+
+            # Calcular la puntuación de somnolencia
+            blink_rate = (total_blinks / (current_time - start_time)) * 60
+            somnolencia_puntuacion = calcular_puntuacion_somnolencia(microsuenos_acumulados, blink_rate, total_yawns, {'microsuenos': 3.0, 'promedio_pestaneos': 0.1, 'bostezos': 0.3})
+
+            #calcular metricas de 60 segundos
+            # Calcular la tasa de parpadeos en los últimos 60 segundos
+            while blink_timestamps and current_time - blink_timestamps[0] > 60:
+                blink_timestamps.popleft()
+
+            blink_rate_60s = len(blink_timestamps)
+
+            # Calcular la tasa de parpadeos en los últimos 60 segundos
+            while yawn_timestamps and current_time - yawn_timestamps[0] > 60:
+                yawn_timestamps.popleft()
+
+            yawn_rate_60s = len(yawn_timestamps)
+
+            results.append({
+                "left_eye_status": str(leftEyeStatus),
+                "right_eye_status": str(rightEyeStatus),
+                "mar": float(mar),  # Asegurar que sea tipo float
+                "yawn_detected": bool(yawn_detected),
+                "somnolencia_puntuacion": float(somnolencia_puntuacion),  # Asegurar que sea tipo float
+                "total_blinks": int(total_blinks),  # Asegurar que sea tipo int
+                "total_yawns": int(total_yawns),  # Asegurar que sea tipo int
+                "microsuenos_acumulados": int(microsuenos_acumulados),  # Asegurar que sea tipo int
+                "blink_rate_60s": float(blink_rate_60s),  # Asegurar que sea tipo float
+                "yawn_rate_60s": float(yawn_rate_60s)  # Asegurar que sea tipo float
+            })
+
+
+        return JSONResponse(content=results)
+
+    except Exception as e:
+        logging.error(f"Error processing image: {str(e)}")
+        raise HTTPException(status_code=500, detail=f"Error processing image: {str(e)}")
+
+
+def detectar_rostros_y_puntos(gray):
+    rects = detector(gray, 0)
+    shapes = [predictor(gray, rect) for rect in rects]
+    return rects, [face_utils.shape_to_np(shape) for shape in shapes]
+
+def procesar_ojos(frame, gray, shape):
+    leftEye = shape[lStart:lEnd]
+    rightEye = shape[rStart:rEnd]
+
+    leftEyeHull = cv2.convexHull(leftEye)
+    rightEyeHull = cv2.convexHull(rightEye)
+    cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
+    cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+
+    leftEyeRegion = gray[leftEye[1][1]:leftEye[4][1], leftEye[0][0]:leftEye[3][0]]
+    rightEyeRegion = gray[rightEye[1][1]:rightEye[4][1], rightEye[0][0]:rightEye[3][0]]
+
+    leftEyeStatus = eye_status(leftEyeRegion)
+    rightEyeStatus = eye_status(rightEyeRegion)
+    return leftEyeStatus, rightEyeStatus
---
+++ Sprint1/Frontend/flutter_application_1/lib/main.dart
@@ -0,0 +1,385 @@
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
+import 'package:audioplayers/audioplayers.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final cameras = await availableCameras();
+  final firstCamera = cameras.first;
+
+  runApp(MyApp(camera: firstCamera));
+}
+
+/*void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Inicializar las cámaras disponibles en el dispositivo
+  final cameras = await availableCameras();
+  
+  // Seleccionar la cámara frontal en lugar de la primera cámara disponible
+  final frontCamera = cameras.firstWhere(
+    (camera) => camera.lensDirection == CameraLensDirection.front,
+  );
+
+  runApp(MyApp(camera: frontCamera));
+}*/
+
+class MyApp extends StatelessWidget {
+  final CameraDescription camera;
+
+  MyApp({required this.camera});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Monitoreo del Conductor en Tiempo Real',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LoadingScreen(camera: camera),
+    );
+  }
+}
+
+class LoadingScreen extends StatefulWidget {
+  final CameraDescription camera;
+
+  LoadingScreen({required this.camera});
+
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  bool isConnected = false;
+  String connectionMessage = "Verificando la conexión al servidor...";
+
+  @override
+  void initState() {
+    super.initState();
+    _checkConnection();
+  }
+
+  Future<void> _checkConnection() async {
+  try {
+    var response = await http
+        .get(Uri.parse('http://10.0.2.2:8000/ping/'))
+        .timeout(Duration(seconds: 3)); // Tiempo de espera de 3 segundos
+
+    if (response.statusCode == 200) {
+      setState(() {
+        isConnected = true;
+        connectionMessage = "Conexión exitosa. Redirigiendo...";
+      });
+      await Future.delayed(Duration(seconds: 2));
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => CameraScreen(camera: widget.camera)),
+      );
+    } else {
+      setState(() {
+        connectionMessage = "Error: No se pudo conectar con el servidor.";
+      });
+      _showConnectionError();
+    }
+  } catch (e) {
+    setState(() {
+      connectionMessage = "Error: No se pudo conectar con el servidor.";
+    });
+    _showConnectionError();
+  }
+}
+
+  void _showConnectionError() {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Error de Conexión'),
+          content: Text('No se pudo conectar con el servidor. Verifica tu conexión e inténtalo de nuevo.'),
+          actions: [
+            TextButton(
+              child: Text('Reintentar'),
+              onPressed: () {
+                Navigator.of(context).pop();
+                _checkConnection();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircularProgressIndicator(),
+            SizedBox(height: 20),
+            Text(connectionMessage, textAlign: TextAlign.center),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CameraScreen extends StatefulWidget {
+  final CameraDescription camera;
+
+  CameraScreen({required this.camera});
+
+  @override
+  _CameraScreenState createState() => _CameraScreenState();
+}
+
+class _CameraScreenState extends State<CameraScreen> {
+  CameraController? _controller;
+  late Future<void> _initializeControllerFuture;
+  bool isSendingFrames = false;
+  Timer? _timer;
+
+  String leftEyeStatus = 'Unknown';
+  String rightEyeStatus = 'Unknown';
+  double mar = 0.0;
+  bool yawnDetected = false;
+  double somnolenciaPuntuacion = 0.0;
+  int totalBlinks = 0;
+  int totalYawns = 0;
+
+  int microsuenosAcumulados= 0;
+  double blinkRate60s= 0.0;
+  double yawnRate60s= 0.0;
+
+  AudioPlayer? audioPlayer;
+  bool isAlertActive = false;
+  bool hasAlertTriggered = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeCamera();
+    audioPlayer = AudioPlayer();
+  }
+
+  Future<void> _initializeCamera() async {
+    _controller = CameraController(
+      widget.camera,
+      ResolutionPreset.low,
+      enableAudio: false,
+    );
+
+    _initializeControllerFuture = _controller!.initialize();
+    await _initializeControllerFuture;
+  }
+
+  void _startOrStopSendingFrames() async {
+  if (isSendingFrames) {
+    // Si el viaje está activo y se presiona "Finalizar viaje"
+    setState(() {
+      isSendingFrames = false;
+      _controller?.stopImageStream();
+    });
+
+    // Enviar la solicitud de finalizar viaje al servidor
+    try {
+      var response = await http.get(Uri.parse('http://10.0.2.2:8000/end_trip/'));  //http://10.0.2.2:8000/detect/
+      if (response.statusCode == 200) {
+        // Respuesta exitosa, resetear las variables
+        setState(() {
+          leftEyeStatus = 'Unknown';
+          rightEyeStatus = 'Unknown';
+          mar = 0.0;
+          yawnDetected = false;
+          somnolenciaPuntuacion = 0.0;
+          totalBlinks = 0;
+          totalYawns = 0;
+          isAlertActive = false;
+          hasAlertTriggered = false;
+          microsuenosAcumulados = 0;
+          blinkRate60s = 0.0;
+          yawnRate60s = 0.0;
+        });
+      } else {
+        print('Error al finalizar el viaje. Status code: ${response.statusCode}');
+      }
+    } catch (e) {
+      print('Error al enviar la solicitud de finalizar viaje: $e');
+    }
+  } else {
+    // Si el viaje no está activo y se presiona "Iniciar viaje"
+    setState(() {
+      isSendingFrames = true;
+      _controller?.startImageStream((CameraImage image) {
+        if (_timer?.isActive ?? false) return;
+        _timer = Timer(Duration(milliseconds: 100), () {
+          _sendFrameToServer(image);
+        });
+      });
+    });
+  }
+}
+
+  Future<void> _sendFrameToServer(CameraImage image) async {
+    try {
+      var request = http.MultipartRequest('POST', Uri.parse('http://10.0.2.2:8000/detect/'));
+      request.files.add(http.MultipartFile.fromBytes('y_plane', image.planes[0].bytes, filename: 'y_plane'));
+      request.files.add(http.MultipartFile.fromBytes('u_plane', image.planes[1].bytes, filename: 'u_plane'));
+      request.files.add(http.MultipartFile.fromBytes('v_plane', image.planes[2].bytes, filename: 'v_plane'));
+      request.fields['width'] = image.width.toString();
+      request.fields['height'] = image.height.toString();
+
+      var response = await request.send();
+
+      if (response.statusCode == 200) {
+        final responseBody = await response.stream.bytesToString();
+        final jsonResponse = jsonDecode(responseBody);
+
+        setState(() {
+          leftEyeStatus = jsonResponse[0]['left_eye_status'];
+          rightEyeStatus = jsonResponse[0]['right_eye_status'];
+          mar = jsonResponse[0]['mar'];
+          yawnDetected = jsonResponse[0]['yawn_detected'];
+          somnolenciaPuntuacion = jsonResponse[0]['somnolencia_puntuacion'];
+          totalBlinks = jsonResponse[0]['total_blinks'];
+          totalYawns = jsonResponse[0]['total_yawns'];
+
+          microsuenosAcumulados = jsonResponse[0]['microsuenos_acumulados'];
+          blinkRate60s = jsonResponse[0]['blink_rate_60s'];
+          yawnRate60s = jsonResponse[0]['yawn_rate_60s'];
+        });
+
+        _checkForAlert();  
+      } else {
+        print('Failed to send frame. Status code: ${response.statusCode}');
+      }
+    } catch (e) {
+      print('Error sending frame: $e');
+    }
+  }
+
+  void _checkForAlert() {
+    if (somnolenciaPuntuacion >= 70 && !isAlertActive && !hasAlertTriggered) {
+      isAlertActive = true;
+      hasAlertTriggered = true;
+      _showAlert();
+    } else if (somnolenciaPuntuacion < 70) {
+      hasAlertTriggered = false;
+    }
+  }
+
+  void _showAlert() {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Center(child: Text('¡Somnolencia Crítica!')),
+          content: Text('Tu nivel de somnolencia es crítico. Detente y descansa.'),
+          actions: [
+            Center(
+              child: ElevatedButton(
+                child: Text('Estoy Despierto'),
+                onPressed: () {
+                  audioPlayer?.stop();
+                  isAlertActive = false;
+                  Navigator.of(context).pop();
+                },
+              ),
+            ),
+          ],
+        );
+      },
+    );
+
+    _playAlarm();  
+  }
+
+  void _playAlarm() {
+    audioPlayer?.setReleaseMode(ReleaseMode.loop);
+
+    audioPlayer?.play(AssetSource('alarma.mp3'));
+
+    Future.delayed(Duration(seconds: 30), () {
+      if (isAlertActive) {
+        audioPlayer?.stop();
+        isAlertActive = false;
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller?.dispose();
+    _timer?.cancel();
+    audioPlayer?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Monitoreo del Conductor en Tiempo Real')),
+      body: Column(
+        children: [
+          Expanded(
+            flex: 2,
+            child: FutureBuilder<void>(
+              future: _initializeControllerFuture,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.done) {
+                  return AspectRatio(
+                    aspectRatio: _controller!.value.aspectRatio,
+                    child: CameraPreview(_controller!),
+                  );
+                } else {
+                  return Center(child: CircularProgressIndicator());
+                }
+              },
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(
+              color: Colors.white,
+              padding: EdgeInsets.all(10),
+              child: _buildStatusDisplay(),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: ElevatedButton(
+              onPressed: _startOrStopSendingFrames,
+              child: Text(isSendingFrames ? 'Finalizar viaje' : 'Iniciar viaje'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildStatusDisplay() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text('Estado del Ojo Izquierdo: $leftEyeStatus'),
+        Text('Estado del Ojo Derecho: $rightEyeStatus'),
+        Text('MAR: $mar'),
+        Text('Bostezo Detectado: $yawnDetected'),
+        Text('Puntuación de Somnolencia: $somnolenciaPuntuacion'),
+        Text('Total de Parpadeos: $totalBlinks'),
+        Text('Total de Bostezos: $totalYawns'),
+        Text('Microsueños Acumulados: $microsuenosAcumulados'),
+        Text('Tasa de Parpadeos (60s): $blinkRate60s'),
+        Text('Tasa de Bostezos (60s): $yawnRate60s'),
+      ],
+    );
+  }
+}
